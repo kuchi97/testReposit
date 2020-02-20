b4bbfce648a5a3a4234a4be446b06ba468b9bab0
encounter <kladsjf@gmail.com>
2020-02-20 14:36:33 +0900

    encounter

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -5,6 +5,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		System.out.println("new Test Branch");
-		System.out.println("push me Master!!!");
+		System.out.println("ENCOUNTER!! pushME!!!");
 	}
 }
